b81497aec79954c7681b4e368e8f5d13e515ff13
btbernstein <34801432+btbernstein@users.noreply.github.com>
2019-04-06 03:27:31 -0600

    Update get_gps.sh

--- get_gps.sh
+++ get_gps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
 ### Install GPS packages and dependencies
-sudo apt-get install gpsd gpsd-clients cmake subversion build-essential espeak freeglut3-dev imagemagick libdbus-1-dev libdbus-glib-1-dev libdevil-dev libfontconfig1-dev libfreetype6-dev libfribidi-dev libgarmin-dev libglc-dev libgps-dev libgtk2.0-dev libimlib2-dev libpq-dev libqt4-dev libqtwebkit-dev librsvg2-bin libsdl-image1.2-dev libspeechd-dev libxml2-dev ttf-liberation -y
+sudo apt-get install gpsd gpsd-clients cmake subversion build-essential espeak freeglut3-dev imagemagick libdbus-1-dev libdbus-glib-1-dev $
 
-sudo reboot now
+sudo pip3 install gpsd-py3
+
+exit 0
