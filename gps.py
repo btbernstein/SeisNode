a63ec1cc7aa8cf6222fc9fe67f20155332d0b979
btbernstein <34801432+btbernstein@users.noreply.github.com>
2019-04-20 19:57:53 -0600

    Create gps.py

---
+++ gps.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+
+times_g = []
+lats = []
+lat_err = []
+lons = []
+lon_err = []
+alts = []
+alt_err = []
+
+while True:
+    # Recive gps data
+    t_g,lat,lon,alt = gps()
+    t_g = str(t_g).split(' ')[1]
+    t_g = str(t_g).split('-')[0]
+    #print(t_g)
+    times_g.append(t_g)
+    
+if __name__ == "name":
