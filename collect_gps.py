c9179e443c318b533a4f31df793181416c67bed1
btbernstein <34801432+btbernstein@users.noreply.github.com>
2019-04-22 15:59:49 -0600

    Update collect_gps.py

--- collect_gps.py
+++ collect_gps.py
@@ -1,4 +1,8 @@
-from g import collect_datum as gps
+from gps import collect_gps as GPS
+from adc import collect_adc as ADC
+
+voltages = []
+g_data = []
 
 times_g = []
 lats = []
@@ -8,10 +12,33 @@ lon_err = []
 alts = []
 alt_err = []
 
-while True:
-    # Recive gps data
-    t_g,lat,lon,alt = gps()
-    t_g = str(t_g).split(' ')[1]
-    t_g = str(t_g).split('-')[0]
-    #print(t_g)
-    times_g.append(t_g)
+
+try:
+    while True:
+        voltages.append(ADC())
+        g_data.append(GPS())
+
+
+except KeyboardInterrupt:
+    for g in g_data:
+        t_g = g[0]
+        t_g = str(t_g).split(' ')[1]
+        t_g = str(t_g).split('-')[0]
+        times_g.append(t_g)
+        
+        lats.append(g[1][0])
+        lat_err.append(g[1][1])
+        
+        lons.append(g[2][0])
+        lon_err.append(g[2][1])
+        
+        alts.append(g[3][0])
+        alt_err.append(g[3][1])
+    
+    arr = [voltages, times_g, lats, lat_err, lons, lon_err, alts, alt_err]
+
+    with open(str(times_g[0]), 'w+') as writeFile:
+        writer = csv.writer(writeFile)
+        writer.writerows(np.array(arr).T)
+
+    writeFile.close()
