ea239e17620c3da7385078e55b7649fa2085bb64
btbernstein <34801432+btbernstein@users.noreply.github.com>
2019-04-22 21:17:05 -0600

    Update get_adc.sh

--- get_adc.sh
+++ get_adc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install build-essential python3-dev python3-smbus python-dev python-smbus python3-pip git libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -y
-git clone https://github.com/caternuson/Adafruit_CircuitPython_ADS1x15.git
+git clone -b fast_read https://github.com/caternuson/Adafruit_CircuitPython_ADS1x15.git
 cd Adafruit_CircuitPython_ADS1x15
 sudo python3 setup.py install
 cd ~
