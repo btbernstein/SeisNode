1ecc01596d77cd8e01c9ae0e9803a3613968d98e
btbernstein <34801432+btbernstein@users.noreply.github.com>
2019-04-22 20:43:42 -0600

    Update get_adc.sh

--- get_adc.sh
+++ get_adc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo apt-get install build-essential python3-dev python3-smbus python-dev python-smbus python3-pip git libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -y
-git clone https://github.com/adafruit/Adafruit_Python_ADS1x15.git
-cd Adafruit_Python_ADS1x15
+git clone https://github.com/caternuson/Adafruit_CircuitPython_ADS1x15.git
+cd Adafruit_CircuitPython_ADS1x15
 sudo python3 setup.py install
 cd ~
 sudo pip3 install  adafruit-blinka RPI.GPIO  adafruit-circuitpython-ads1x15
