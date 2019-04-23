556b90e32b87d0d16a3babbca7887b541d554514
btbernstein <34801432+btbernstein@users.noreply.github.com>
2019-04-22 23:09:19 -0600

    Update adc.py

--- adc.py
+++ adc.py
@@ -4,7 +4,6 @@ import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
-import csv
 
 # Create the I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -12,18 +11,30 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # Create the ADC object using the I2C bus
 ads = ADS.ADS1115(i2c)
 ads.gain = 16
+ads.mode = 0x0000
+ads.data_rate = 860
+ads.comparator_config = 0
 # Create single-ended input on channel 0
-chan = AnalogIn(ads, ADS.P0)
+chan0 = AnalogIn(ads, ADS.P0)
 
 # Arrays to store volages
 voltages = []
 
 def collect_adc():
-    # Recieve voltage values    
-    return chan.voltage
+    try:
+        with chan0 as chan:
+            # Recieve voltage values          
+            voltages.append(chan.voltage)
+    except:
+        return voltages
 
+def main():
+    t0 = time.time()
+    voltages = collect_adc()
+    t1 = time.time()
+    dt = t1 - t0
+    print(len(voltages)/dt)
 
-    
 if __name__ == "__main__":
-    collect_adc()
+    main()
     
