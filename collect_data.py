9ad571da9f97a0c47102603cb2106b67fe42ed27
btbernstein <34801432+btbernstein@users.noreply.github.com>
2019-04-22 23:11:33 -0600

    Update collect_data.py

--- collect_data.py
+++ collect_data.py
@@ -1,5 +1,8 @@
 from gps import collect_gps as GPS
 from adc import collect_adc as ADC
+import csv
+import time
+import numpy as np
 
 voltages = []
 g_data = []
@@ -12,33 +15,36 @@ lon_err = []
 alts = []
 alt_err = []
 
+print("Press enter to collect data:")
+pause = input('')
+print("Collecting data...")
 
-try:
-    while True:
-        voltages.append(ADC())
-        g_data.append(GPS())
+g_data.append(GPS())
+t0 = time.time()
+voltages = ADC()
+t1 = time.time()
+g_data.append(GPS())
 
 
-except KeyboardInterrupt:
-    for g in g_data:
-        t_g = g[0]
-        t_g = str(t_g).split(' ')[1]
-        t_g = str(t_g).split('-')[0]
-        times_g.append(t_g)
-        
-        lats.append(g[1][0])
-        lat_err.append(g[1][1])
-        
-        lons.append(g[2][0])
-        lon_err.append(g[2][1])
-        
-        alts.append(g[3][0])
-        alt_err.append(g[3][1])
+for g in g_data:
+    t_g = g[0]
+    t_g = str(t_g).split(' ')[1]
+    t_g = str(t_g).split('-')[0]
+    times_g.append(t_g)
+
+    lats.append(g[1][0])
+    lat_err.append(g[1][1])
+
+    lons.append(g[2][0])
+    lon_err.append(g[2][1])
+
+    alts.append(g[3][0])
+    alt_err.append(g[3][1])
     
-    arr = [voltages, times_g, lats, lat_err, lons, lon_err, alts, alt_err]
+arr = [voltages, times_g, lats, lat_err, lons, lon_err, alts, alt_err]
 
-    with open(str(times_g[0]), 'w+') as writeFile:
-        writer = csv.writer(writeFile)
-        writer.writerows(np.array(arr).T)
+with open("data/"+str(times_g[0]), 'w+') as writeFile:
+    writer = csv.writer(writeFile)
+    writer.writerows(np.array(arr).T)
 
-    writeFile.close()
+writeFile.close()
