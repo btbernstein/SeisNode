ae9b696fa95c16bcfbcaf3d6831bf66117e50995
btbernstein <34801432+btbernstein@users.noreply.github.com>
2019-05-07 16:18:13 -0600

    Create collect_adc.py

---
+++ collect_adc.py
@@ -0,0 +1,25 @@
+import numpy as np
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+# Create the I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the ADC object using the I2C bus
+ads = ADS.ADS1115(i2c)
+ads.gain = 4
+ads.mode = 0x0000
+ads.data_rate = 860
+ads.comparator_config = 0
+# Create single-ended input on channel 0
+chan0 = AnalogIn(ads, ADS.P0)
+
+with open("/home/pi/Rpi/data/adc", "w+") as f:
+    with chan0 as chan:
+        print("Collecting")
+        while True:
+            # Recieve voltage values
+            f.write(str(chan.voltage) + "\n")
