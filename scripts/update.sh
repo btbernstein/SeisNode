69ca4644a38f9ed0a892d2db56c81ce0c4942c86
btbernstein <34801432+btbernstein@users.noreply.github.com>
2019-05-07 16:13:24 -0600

    Update and rename update.sh to scripts/update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+
 cd ~/
 sudo rm -rf Rpi
-git clone https://github.com/btbernstein/Rpi.git
 cd /usr/local/lib/python3.5/dist-packages
 sudo rm -rf adafruit_ads1x15
-cd ~/Rpi
+
+git clone https://github.com/btbernstein/Rpi.git
+cd ~/Rpi/scripts
 sudo chmod +x get_software.sh
 ./get_software.sh
 
 sudo chmod +x change_rc.sh
 ./change_rc.sh
+
+cd ~/
 
 exit 0
