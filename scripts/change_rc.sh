5baefc4d69974aeb08cdfe46f888e2790dbccf74
btbernstein <34801432+btbernstein@users.noreply.github.com>
2019-05-07 16:42:31 -0600

    Update change_rc.sh

--- scripts/change_rc.sh
+++ scripts/change_rc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd ~/
+
 sudo mv /etc/rc.local /etc/rc_copy.local
-sudo mv Rpi/rc.local /etc/rc.local
+sudo mv /home/pi/Rpi/rc.local /etc/rc.local
 sudo chmod 777 /etc/rc.local
 
 exit 0
