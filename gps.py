1cde21f85112c206d860a074c07e1160d917d515
btbernstein <34801432+btbernstein@users.noreply.github.com>
2019-05-08 16:32:34 -0600

    Update gps.py

--- gps.py
+++ gps.py
@@ -5,7 +5,7 @@ def collect_gps():
     gpsd.connect()
     packet = gpsd.get_current()
     #print(packet.mode)
-    if packet.mode >= 3:     
+    if packet.mode >= 3:
         time = packet.get_time(local_time=True)
         lat = packet.lat
         ey = packet.error['y']
@@ -15,8 +15,21 @@ def collect_gps():
         ez = packet.error['v']
 
         const = 1/111319.9 #  degrees per meter
-        data = [time,(lat,ey*const),(lon,ex*const),(alt,ez)]
+        data = [time,lat,ey*const,lon,ex*const,alt,ez]
         return data
-        
+
+def main():
+    import csv
+    import time
+    with open("/home/pi/Rpi/data/gps.csv", "w+") as writefile:
+        writer = csv.writer(writefile)
+        writer.writerow(["Time", "Lat", "Lat Err", "Lon", "Lon Err", "Alt", "Alt Err"])
+        while True:
+        # Recieve GPS values
+            data = collect_gps()
+            writer.writerow(data)
+            time.sleep(1)
+
 if __name__ == "__main__":
-    collect_gps()
+    main()
+
