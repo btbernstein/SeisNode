26db3f9250d79614b437e04c69d7630f83e82d65
btbernstein <34801432+btbernstein@users.noreply.github.com>
2019-05-09 00:57:19 -0600

    Update led.py

--- master/led.py
+++ master/led.py
@@ -1,21 +1,32 @@
 import RPi.GPIO as GPIO
 import time
 
-pin = 18 
+pin = 18
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, GPIO.OUT)  
-GPIO.output(pin, GPIO.HIGH)
+def pulse():
+    GPIO.output(pin,GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(pin,GPIO.LOW)
+    time.sleep(0.5)
+
+def initialize():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(pin, GPIO.OUT)
+
+initialize()
 
 def turn_on():
+    initialize()
     GPIO.output(pin, GPIO.HIGH)
- 
-def turn_off():
-    GPIO.output(pin, GPIO.LOW)
+    time.sleep(1)
+    #GPIO.cleanup()
 
-def blink():
-  while True:
-    GPIO.output(pin, GPIO.HIGH)
-    time.sleep(1)
+def turn_off():
+    #initialize()
     GPIO.output(pin, GPIO.LOW)
     time.sleep(1)
+
+if __name__ == "__main__":
+    for i in range(2):
+        pulse()
