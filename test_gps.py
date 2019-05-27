af0ba47e3c2df1579b2447370b42af2d76db6b36
btbernstein <34801432+btbernstein@users.noreply.github.com>
2019-05-27 14:30:37 -0600

    Update test_gps.py

--- test_gps.py
+++ test_gps.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import gpsd
 import time
+import led
 
 def test_gps():
     while True:
@@ -9,6 +10,8 @@ def test_gps():
             packet = gpsd.get_current()
             if packet.mode >= 3:
                 print("GPS Online.")
+                for i in range(2):
+                    led.pulse()
                 return
         except:
             time.sleep(0.5)
