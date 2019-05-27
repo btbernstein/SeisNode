0a32c6ee9890c2640f0359416a98c32bf501cd1d
btbernstein <34801432+btbernstein@users.noreply.github.com>
2019-05-27 13:07:26 -0600

    Update trigger.py

--- trigger.py
+++ trigger.py
@@ -2,7 +2,11 @@ import smbus
 import time
 from gps import collect_gps as GPS
 import led
+import numpy as np
 
+bus = smbus.SMBus(1)
+shot = 0
+status = True
 while True:
     #Parameters for write_byte_data
     #1. Address of the device
@@ -17,43 +21,37 @@ while True:
     data = bus.read_i2c_block_data(0x1D, 0x00, 7)
 
     number_of_bits = 16
-#    MSB_x = data[1]
-#    LSB_x = data[2]
-#    MSB_y = data[3]
-#    LSB_y = data[4]
     MSB_z = data[5]
     LSB_z = data[6]
 
-#    xAccl = (MSB_x * 256 + LSB_x) / number_of_bits
-#    if xAccl > 2047:
-#        xAccl -= 4096
-#    yAccl = (MSB_y * 256 + LSB_y) / number_of_bits
-#    if yAccl > 2047:
-#        yAccl -= 4096
     zAccl = (MSB_z * 256 + LSB_z) / number_of_bits
     if zAccl > 2047:
         zAccl -= 4096
-
-    shot = 0
+        
     # if z acceleration changes by some great amount
-    if abs((prev_z-zAccl)/zAccl) >= 0.3: ### Change this
+    if abs((prev_z-zAccl)/zAccl) >= 1.0: ### Change this
+        bus.write_byte_data(0x1D, 0x2A, 0)
         # record GPS for 1 second.
-        with open("/home/pi/Rpi/data/shots/shot%i.csv" %shot, "w+") as writefile:
-            status = False
-            for i in range(4):
-                if status:
-                    led.turn_off():
-                else:
-                    led.turn_on()
-
-                writer = csv.writer(writefile)
-                writer.writerow(["Time", "Lat", "Lat Err", "Lon", "Lon Err", "Alt", "Alt Err"])
-
-                data = collect_gps()
+        with open("/home/pi/Rpi/data/shots.csv" %shot, "a+") as writefile:
+            print("Trigger")
+            led.turn_on()
+            writer = csv.writer(writefile)
+            if shot == 0:
+                writer.writerow(["Shot","Time", "Lat", "Lat Err", "Lon", "Lon Err", "Alt", "Alt Err"])
+            start = time.time()
+            while time.time()-start < 1:
+                data = GPS()
+                data = np.concatenate([[shot],data])
                 writer.writerow(data)
-                writefile.flush()
+                time.sleep(0.2)
+            writefile.flush()
             writefile.close()
+
             shot += 1
-    
+            time.sleep(30)
+            led.turn_off()
+            prev_z = 1000
+            status = True
+            continue
+    status = False
     prev_z = zAccl
-    
