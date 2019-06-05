1d1895c8f72dfe3d599b6b42f50170ad481a14dd
btbernstein <34801432+btbernstein@users.noreply.github.com>
2019-06-04 23:03:35 -0600

    Update collect_adc.py

--- collect_adc.py
+++ collect_adc.py
@@ -4,6 +4,7 @@ import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
+import datetime
 
 # Create the I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
