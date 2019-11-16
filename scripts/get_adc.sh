c2e164c8509085958900bc3ccea200b1a4483cce
btbernstein <34801432+btbernstein@users.noreply.github.com>
2019-11-16 09:01:24 -0700

    Update get_adc.sh

--- scripts/get_adc.sh
+++ scripts/get_adc.sh
@@ -2,10 +2,10 @@
 
 sudo apt-get install build-essential python3-dev python3-smbus python-dev python-smbus python3-pip git libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -y
 
-cd ~/Rpi
-git clone -b fast_read https://github.com/caternuson/Adafruit_CircuitPython_ADS1x15.git
-cd ~/Rpi/Adafruit_CircuitPython_ADS1x15
-sudo python3 setup.py install
+#cd ~/Rpi
+#git clone -b fast_read https://github.com/caternuson/Adafruit_CircuitPython_ADS1x15.git
+#cd ~/Rpi/Adafruit_CircuitPython_ADS1x15
+#sudo python3 setup.py install
 
 cd ~/
 
