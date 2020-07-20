e9816847f49f4ea87fed2265804f4e29ed68cd73
btbernstein <34801432+btbernstein@users.noreply.github.com>
2020-07-20 00:31:55 -0600

    Update get_software.sh

--- scripts/get_software.sh
+++ scripts/get_software.sh
@@ -3,15 +3,15 @@
 sudo pip install --upgrade pip
 sudo pip3 install --upgrade pip
 
-sudo chmod +x /home/pi/Rpi/scripts/get_gps.sh
-. /home/pi/Rpi/scripts/get_gps.sh
-sudo chmod +x /home/pi/Rpi/scripts/get_adc.sh
-. /home/pi/Rpi/scripts/get_adc.sh
-sudo chmod +x /home/pi/Rpi/scripts/change_rc.sh
-. /home/pi/Rpi/scripts/change_rc.sh
-sudo chmod +x /home/pi/Rpi/scripts/start_sensors.sh
-. /home/pi/Rpi/scripts/start_sensors.sh
-sudo chmod +x /home/pi/Rpi/scripts/lcd.sh
-. /home/pi/Rpi/scripts/lcd.sh
-
-exit 0
+sudo chmod +x /home/pi/SeisNode/scripts/get_gps.sh
+. /home/pi/SeisNode/scripts/get_gps.sh
+sudo chmod +x /home/pi/SeisNode/scripts/get_adc.sh
+. /home/pi/SeisNode/scripts/get_adc.sh
+sudo chmod +x /home/pi/SeisNode/scripts/change_rc.sh
+. /home/pi/SeisNode/scripts/change_rc.sh
+sudo chmod +x /home/pi/SeisNode/scripts/start_sensors.sh
+. /home/pi/SeisNode/scripts/start_sensors.sh
+sudo chmod +x /home/pi/SeisNode/scripts/lcd.sh
+. /home/pi/SeisNode/scripts/lcd.sh
+sudo chmod +x /home/pi/SeisNode/scripts/startup.py
+. /home/pi/SeisNode/scripts/startup.sh
