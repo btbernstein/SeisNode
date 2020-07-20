06163029131a5fdbfa76f28c090ff8af0003a8ff
btbernstein <34801432+btbernstein@users.noreply.github.com>
2020-07-19 21:38:01 -0600

    Update change_rc.sh

--- scripts/change_rc.sh
+++ scripts/change_rc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo mv /etc/rc.local /etc/rc_copy.local
-sudo mv /home/pi/Rpi/scripts/assetsrc.local /etc/rc.local
+sudo mv /home/pi/Rpi/scripts/assets/rc.local /etc/rc.local
 sudo chmod 777 /etc/rc.local
-echo "rc" >> assets/_status.txt
+echo "rc" >> /home/pi/Rpi/scripts/assets/_status.txt
