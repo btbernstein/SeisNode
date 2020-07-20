417f6afbcb4c555ee2c3ebeb083bbf840b2a0baa
btbernstein <34801432+btbernstein@users.noreply.github.com>
2020-07-20 00:38:15 -0600

    Update get_software.sh

--- scripts/get_software.sh
+++ scripts/get_software.sh
@@ -5,13 +5,17 @@ sudo pip3 install --upgrade pip
 
 sudo chmod +x /home/pi/SeisNode/scripts/get_gps.sh
 . /home/pi/SeisNode/scripts/get_gps.sh
+
 sudo chmod +x /home/pi/SeisNode/scripts/get_adc.sh
 . /home/pi/SeisNode/scripts/get_adc.sh
-sudo chmod +x /home/pi/SeisNode/scripts/change_rc.sh
-. /home/pi/SeisNode/scripts/change_rc.sh
+
 sudo chmod +x /home/pi/SeisNode/scripts/start_sensors.sh
 . /home/pi/SeisNode/scripts/start_sensors.sh
+
 sudo chmod +x /home/pi/SeisNode/scripts/lcd.sh
 . /home/pi/SeisNode/scripts/lcd.sh
+
 sudo chmod +x /home/pi/SeisNode/scripts/startup.py
-. /home/pi/SeisNode/scripts/startup.sh
+
+sudo chmod +x /home/pi/SeisNode/scripts/change_rc.sh
+. /home/pi/SeisNode/scripts/change_rc.sh
