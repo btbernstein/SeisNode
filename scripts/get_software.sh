abe608d40932d85d5416b5669fa1bef6be450cea
btbernstein <34801432+btbernstein@users.noreply.github.com>
2020-07-27 11:39:26 -0600

    Update get_software.sh

--- scripts/get_software.sh
+++ scripts/get_software.sh
@@ -12,10 +12,10 @@ sudo chmod +x /home/pi/SeisNode/scripts/get_adc.sh
 sudo chmod +x /home/pi/SeisNode/scripts/start_sensors.sh
 . /home/pi/SeisNode/scripts/start_sensors.sh
 
-sudo chmod +x /home/pi/SeisNode/scripts/lcd.sh
-. /home/pi/SeisNode/scripts/lcd.sh
-
 sudo chmod +x /home/pi/SeisNode/SeisNode/startup.py
 
 sudo chmod +x /home/pi/SeisNode/scripts/change_rc.sh
 . /home/pi/SeisNode/scripts/change_rc.sh
+
+sudo chmod +x /home/pi/SeisNode/scripts/lcd.sh
+. /home/pi/SeisNode/scripts/lcd.sh
